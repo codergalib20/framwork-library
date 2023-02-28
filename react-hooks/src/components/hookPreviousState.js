3b8161e41b4d3e6407da226a4456e6e5e868d201
codergalib20 <coderboygalif@gmail.com>
2023-02-28 06:19:27 +0600

    commited

--- react-hooks/src/components/hookPreviousState.js
+++ react-hooks/src/components/hookPreviousState.js
@@ -10,6 +10,9 @@ export default function HookPreviousState() {
     }
   };
 
+  const code =
+    'import { useState } from "react"; \n export default function HookPreviousState() { const initialCount = 0; const [count, setCount] = useState(initialCount);  const increment = () => { for (let i = 0; i < 5; i++) {  setCount((prevCount) => prevCount + 1); }};const decrement = () => {if (count >= 3) {for (let i = 0; i < 3; i++) {setCount((prevCount) => prevCount - 1);}} else {alert("Number less then decrement");}};console.log(initialCount);return (<div><h2>Hooks Previous State value</h2>Count : {count}<button onClick={() => setCount(0)}>Reset</button><button onClick={increment}>Increment</button><button onClick={decrement}>Decrement</button></div>);} ';
+
   const decrement = () => {
     if (count >= 3) {
       for (let i = 0; i < 3; i++) {
@@ -27,6 +30,7 @@ export default function HookPreviousState() {
       <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <div>{code}</div>
     </div>
   );
 }
